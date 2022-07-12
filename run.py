9782133bed8c8d898144cd7bb4049280ea4cd8c4
Joee Dinatha Xavier <102127928+JoeDevXX2K@users.noreply.github.com>
2022-07-12 11:54:06 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python2
+#coding=utf-8
+
+import requests, re, os, time
+
+_____kontol_lu_bau_____ = print
+_____joeganteng_____ = input
+_____koncol_____ = os.system("clear")
+
+JANDA = "\x1b[1;91m" # warna janda
+KONTOL = "\x1b[1;97m" # GATAU
+
+def _____bauu_____():
+    _____koncol_____
+    _____kontol_lu_bau_____("")
+    _____kontol_lu_bau_____("")
+    _____kontol_lu_bau_____("")
+    _____kontol_lu_bau_____(f"""          {JANDA}88b 88  dP""b8 888888 88b 88 888888  dP"Yb  888888 
+          88Yb88 dP   `" 88__   88Yb88   88   dP   Yb   88   
+          88 Y88 Yb  "88 88""   88 Y88   88   Yb   dP   88   
+          88  Y8  YboodP 888888 88  Y8   88    YbodP    88 {KONTOL}""")
+    _____kontol_lu_bau_____("")
+    _____kontol_lu_bau_____("")
+    _____kontol_lu_bau_____(" [!] Bentar lagi gwe up scnya sabar kon..")
+    ___hallo___ = _____joeganteng_____("\n [*] kembali ");os.system('xdg-open https://github.com/JoeDevXX2K');time.sleep(2);exit()
+    
+_____bauu_____()
