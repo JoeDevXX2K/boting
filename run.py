ff00d5204016963541b7b5c38e65991e698ffaff
Joee Dinatha Xavier <102127928+JoeDevXX2K@users.noreply.github.com>
2022-07-12 11:52:54 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 
+# By Joee Dinatha Xavier
+
+import os,random,sys,time,json,requests
+from time import sleep
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
+from bs4 import BeautifulSoup as parser
+import os,sys,time,random,json,requests
+from rich import print as prints
+from rich.panel import Panel
+import random,sys
+import requests, os, re, bs4, sys, json, time, random, datetime, subprocess, logging, base64, marshal
+from concurrent.futures import ThreadPoolExecutor as Lampung
+from datetime import datetime
+from bs4 import BeautifulSoup as parser
+from time import sleep as jeda
+
+### WARNA RANDOM ###
+P = '\x1b[1;97m' # PUTIH
+M = '\x1b[1;91m' # MERAH
+H = '\x1b[1;92m' # HIJAU
+K = '\x1b[1;93m' # KUNING
+B = '\x1b[1;94m' # BIRU
+U = '\x1b[1;95m' # UNGU
+O = '\x1b[1;96m' # BIRU MUDA
+N = '\x1b[0m'    # WARNA MATI
+A = '\x1b[1;90m' # WARNA ABU ABU
+BN = '\x1b[1;107m' # BELAKANG PUTIH
+BBL = '\x1b[1;106m' # BELAKANG BIRU LANGIT
+BP = '\x1b[1;105m' # BELAKANG PINK
+BB = '\x1b[1;104m' # BELAKANG BIRU
+BK = '\x1b[1;103m' # BELAKANG KUNING
+BH = '\x1b[1;102m' # BELAKANG HIJAU
+BM = '\x1b[1;101m' # BELAJANG MERAH
+BA = '\x1b[1;100m' # BELAKANG ABU ABU
+my_color = [
+ P, M, H, K, B, U, O, N]
+warna = random.choice(my_color)
+
+def tik():
+    titik = ['.   ','..  ','... ']
+    for o in titik:
+        print("menghapus")
+        sys.stdout.flush();jeda(1)
+
+def _____Jalan_____(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.005)
+
+_____LUKONTOL_________ = print
+_____JOEEGANTENG_____ = input
+
+
+def _logo_():
+    _____Jalan_____(f"""                         {U} _ _   _______            
+           /\            (_|_) |__   __|           
+          /  \   ___  ___ _ _     | |_      _____  
+         {K}/ /\ \ / __|/ __| | |    | \ \ /\ / / _ \ 
+       {M} / ____ \\__ \ (__|  | |    | |\ V  V / (_) |
+       /_/    \_\___/\___|_|_|    |_| \_/\_/ \___/""");time.sleep(1)
+    _____Jalan_____(f"       {P}Multi Brute Force Facebook {K}0.6 {P}By {K}Joee Dev{P}")
+
+def ___welcome___():
+    _____LUKONTOL_________(f"           {P}[{K}•{P}] {P}Hello   : {H}Jaya Budi!{P}");time.sleep(1)
+    _____LUKONTOL_________(f"           {P}[{K}•{P}] {P}User    : {H}Premium{P}");time.sleep(1)
+    _____LUKONTOL_________(f"           {P}[{K}•{P}] {P}Expired : {M}29 {P}More day");time.sleep(1)
+
+def ___menu___():
+    os.system("clear")
+    _____LUKONTOL_________("")
+    _logo_()
+    print("")
+    ___welcome___()
+    _____LUKONTOL_________("")
+    _____LUKONTOL_________ ("")
+    _____Jalan_____(f"     {B}[{K}01{B}]. {P}Crack Dari ID Teman {P}( {H}Masall {P})\n     {B}[{K}02{B}]. {P}Crack Dari ID Teman Publik\n     {B}[{K}03{B}]. {P}Crack Dari Followers Publik\n     {B}[{K}04{B}]. {P}Crack Dari Komentar Post\n     {B}[{K}05{B}]. {P}Crack Dari Grup\n     {B}[{K}06{B}]. {P}Crack Dari ID Random\n     {B}[{K}07{B}].{P} Crack Dari File\n     {B}[{K}08{B}]. {P}Crack Hasil Crack\n     {B}[{K}09{B}]. {P}Settings UserAgent\n     {B}[{K}10{B}]. {P}Upgrade {H}Pro\n     {B}[{M}00{B}]. {P}Log Out {P}( {M}Hapus Cookie {P})")
+    _____LUKONTOL_________("")
+    _____ANJING___ = _____JOEEGANTENG_____(f" {P}[{H}*{P}] choose : ");tik()
+    if _____ANJING___ in ("01","1"):
+      _____crackv1_____()
+    elif _____ANJING___ in ("02","2"):
+        _____LUKONTOL_________("kontol")
+        
+    else:
+        _____LUKONTOL_________(f" {P}[{M}•{P}] Input Yang Bener Kontol");time.sleep(2);___menu___()
+
+
+
+
+def _____crackv1_____():
+    prints(Panel('   [white]Anda Bisa Mengetik [green]"me" [white]Untuk Crack Dari Daftar Teman Sendiri'))
+    _____ANJING___ = _____JOEEGANTENG_____(f" {P}[{K}+{P}] ID Target : {H}");time.sleep(2)
+    _____LUKONTOL_________(f" {P}[{M}!{P}] Collecting ID : {M}4729");time.sleep(3)
+    _____LUKONTOL_________(f" {P}[{H}✓{P}] Berhasil : {H}4729{P}")
+    #_____LUKONTOL_________("")
+    prints(Panel("                [blue][[yellow]1[blue]] [white]Validate [blue][[yellow]2[blue]][white] Regular [blue][[yellow]3[blue]][white] Api FB",title='[bold green]Method'))
+    _____ANJING___ = _____JOEEGANTENG_____(f"  {P}└──> {P}")
+    prints(Panel("                [blue][[yellow]1[blue]][white] Free FB  [blue][[yellow]2[blue]] [white]Mbasic  [blue][[yellow]3[blue]] [white]Mobile",title='[bold green]Login'))
+    _____ANJING___ = _____JOEEGANTENG_____(f"  {P}└──> {P}")
+    prints(Panel("                [blue][[yellow]1[blue]][white] ID Tua   [blue][[yellow]2[blue]] [white]ID Muda [blue][[yellow]3[blue]] [white]ID Acak",title='[bold green]Urutan ID'))
+    _____ANJING___ = _____JOEEGANTENG_____(f"  {P}└──> {P}")
+    _____LUKONTOL_________("")
+    _____LUKONTOL_________(f" {P}[{H}•{P}] Cek Opsi Akun {K}CP {P}?{P} [Y/t]")
+    _____ANJING___ = _____JOEEGANTENG_____(f"  {P}└──> {H}")
+    _____LUKONTOL_________(f" {P}[{H}•{P}] Password Manual [ {K}Create {P}]")
+    _____ANJING___ = _____JOEEGANTENG_____(f"  {P}└──> {H}")
+    _____LUKONTOL_________(f" {P}[{H}•{P}] Password Manual [{K} Belakang Password{P} ]")
+    _____ANJING___ = _____JOEEGANTENG_____(f"  {P}└──> {H}")
+    _____LUKONTOL_________("")
+    _____LUKONTOL_________(f"                  {M}•{K}•{H}•{P}   Proses Crack Dimulai   {M}•{K}•{H}•{P}");time.sleep(1)
+    _____Jalan_____(f"      [ {H}Hasil Ok Tersimpan Ke : Results/Ok-Jumat-8-Juli-2022{P} ]")
+    _____Jalan_____(f"      [ {K}Hasil Cp Tersimpan Ke : Results/Cp-Jumat-8-Juli-2022{P} ]");time.sleep(3)
+    print("")
+    print(f"  {H}*──> 1108419161 • katasandi • 10 Oktober 1999")
+    print(f"  {K}*──> 1786231312 • eti fujrianti • 12 Desember 2004")
+    print(f"  {K}*──> 1373066396 • fauziah123 • 19 Mei 2004 ")
+    print(f"  {H}*──> 662588673 • naura12345 • 09 Februari 2000")
+    print(f"  {K}*──> 514436077 • sayang • 03 Juni 2002")
+    print(f"  {K}*──> 100067902379299 • amad123 • 12 Juni 2001")
+    print(f"  {K}*──> 100036798831884 • cahaya123 • 05 Desember 2004")
+    print(f"  {H}*──> 100044794492664 • kontol • 04 Oktober 2002")
+    print(f"  {H}*──> 755033393 • lusi okta nurhayani • 10 Desember 2002")
+    print(f"  {H}*──> 100078196247516 • saiful1234 • 19 Desember 2007")
+    print(f"  {K}*──> 737957977 • ricky123 • 20 Oktober 2003")
+    print(f"  {H}*──> 100015401071761 • juna idi • 02 Desember 2009")
+    print(f"  {H}*──> 100034561094478 • haikal oa • 23 Juni 2006")
+    print(f"  {H}*──> 1686769318 • sikman messa • 29 Februari 2001")
+    print(f"  {K}*──> 100034026761735 • didi hasmiranda • 30 Juni 2005")
+    print(f"  {H}*──> 1728329068 • zahra123 • 01 Maret 1999")
+    print(f"  {H}*──> 100033861067061 • azwar123 • 15 Maret 2004")
+    print(f"  {H}*──> 100074744749772 • anjing • 14 Oktober 2005")
+    print(f"  {H}*──> 100076303737502 • raisha1234 • 17 Januari 2000")
+    print(f"  {H}*──> 100065312199173 • anjing • 06 April 2007")
+    print(f"  {K}*──> 100024560047581 • dinda dinda • 09 Agustus 1979")
+    print(f"  {K}*──> 100052249155444 • rival dm • 30 November 1944")
+    print(f"  {H}*──> 100028852071702 • indri1234 • 19 Juli 1955")
+    print(f"  {H}*──> 100057145133134 • anisa123 • 11 Juni 2001")
+    print(f"  {H}*──> 100022935972517 • sahroni123 • 18 September 1970")
+    print(f"  {H}*──> 100062175872849 • pedes geliga • 02 Oktober 2003")
+    print(f"  {P}[{K}0:07:20{P}] {P}[{K}828/4729{P}] {P}[OK:-{H}17{P}] [CP:-{K}9{P}]");time.sleep(999)
+    
+___menu___()    
